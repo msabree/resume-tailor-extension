659c49b7b4f48e5d234151a4f28ac575ae0a7968
Makeen Sabree <makeen.sabree@gmail.com>
2024-11-12 08:43:00 -0500

    Remove public path.

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,6 @@ module.exports = {
     output: {
         path: path.join(__dirname, "dist/js"),
         filename: "[name].js",
-        publicPath: "/"
     },
     externals: {
         canvg: "canvg",
